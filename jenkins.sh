b32e0d3b2cbecab314bab7b84bc4583f22c9ba58
Colm Vize <colm@artomatix.com>
2017-08-16 14:03:26 +0100

    First version of jenkins script

---
+++ jenkins.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+
+TOP_DIR=$(pwd)
+NUNIT="$(pwd)/hooks/binfiles/NUnit-2.6.4/bin/nunit-console.exe"
+NUNIT_ARGS='-noshadow -exclude "JenkinsDisable" -xml=nunit-result.xml -nologo'
+
+function quitOnFailure
+{
+    if [ ! $? -eq 0  ]; then exit 1
+    fi
+}
+
+function doTests
+{
+        mono $NUNIT $1 $NUNIT_ARGS;
+        quitOnFailure;
+}
+
+cd ${TOP_DIR}
+#git reset .
+#git checkout .
+#git clean -dxf
+
+
+cd src_c
+mkdir build
+cd build
+
+cmake .. && make -j4 && make aitest
+quitOnFailure
+
+cd ${TOP_DIR}
+
+cd bindings/csharp
+
+xbuild ArtomatixImageLoader.sln
+doTests ArtomatixImageLoaderTests/bin/x64/Debug/ArtomatixImageLoaderTests.dll
+
+
+
+
+
+
+exit 0
